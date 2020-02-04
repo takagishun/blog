31183e171a5e2120df7e0e2ef753d8c16541c8af
shun takagi <s.takagi.910@gmail.com>
2020-02-04 22:07:26 +0900

    fix routing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,7 +56,7 @@ class PostsController < ApplicationController
 
 	# Use callbacks to share common setup or constraints between actions.
 	def set_post
-		@post = Post.find(params[:id])
+		@post = Post.find_by(url: params[:url])
 	end
 
 	# Never trust parameters from the scary internet, only allow the white list through.
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	validates :url, presence: true
+
+	def to_param
+		url
+	end
 end
