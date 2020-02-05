8328709eef7362cda09f4c970f4c734adadfd0f6
shun takagi <s.takagi.910@gmail.com>
2020-02-05 11:11:43 +0900

    imageアップロード先をS3に変更 ミス修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,9 +10,9 @@ class ImageUploader < CarrierWave::Uploader::Base
       storage :file
     else
       storage :fog
-    end
+   end
 
-  # Override the directory where uploaded files will be stored.
+   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
